01d2a6ed65d6d3ec6e8d62e0bea3cfa327627e0d
Andrea <andrea@zampierolo.dev>
2021-12-06 14:47:57 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Meteo',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;

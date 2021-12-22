842bc2641c52f5abed8b39988126342ab4567ebf
Andrea <andrea@zampierolo.dev>
2021-12-22 10:03:03 +0100

    get correct weather icon

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,3 +4,15 @@ import moment from "moment";
 export const getDataFormatted = (unixDate, dateFormat) => {
 	return moment.unix(unixDate).format(dateFormat)
 }
+
+export const getWeatherIcon = (icon) => {
+	console.log('icon :>> ', icon);
+	switch (icon) {
+		case 'partly-cloudy-day':
+			return 'day-cloudy'
+	
+		default:
+			return icon
+	}
+
+}

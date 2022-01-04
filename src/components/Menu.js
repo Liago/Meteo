4cad3fe442edf342469ea5ccedbde4371c704a6b
Andrea <andrea@zampierolo.dev>
2022-01-04 16:53:46 +0100

    cleaned menu after fixed components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,6 +12,8 @@ import {
 import "./Menu.css";
 import { useContext } from "react";
 import { useSelector } from "react-redux";
+import MenuLocations from "./menu/menuLocations";
+import { MenuItems } from "./menu/menuItems";
 
 
 const appPages = [
@@ -27,37 +29,22 @@ const appPages = [
 		iosIcon: paperPlaneOutline,
 		mdIcon: paperPlaneSharp,
 	},
-	// {
-	// 	title: "Favorites",
-	// 	url: "/page/Favorites",
-	// 	iosIcon: heartOutline,
-	// 	mdIcon: heartSharp,
-	// },
-	// {
-	// 	title: "Archived",
-	// 	url: "/page/Archived",
-	// 	iosIcon: archiveOutline,
-	// 	mdIcon: archiveSharp,
-	// },
-	// {
-	// 	title: "Trash",
-	// 	url: "/page/Trash",
-	// 	iosIcon: trashOutline,
-	// 	mdIcon: trashSharp,
-	// },
-	// {
-	// 	title: "Spam",
-	// 	url: "/page/Spam",
-	// 	iosIcon: warningOutline,
-	// 	mdIcon: warningSharp,
-	// },
 ];
 
 const Menu = () => {
-	const location = useLocation();
+	
 	const { locations } = useSelector(state => state.app);
-
 	const { toggle, toggleFunction } = useContext(ThemeContext);
+
+	const renderLocationsSaved = () => {
+		if (!locations) return;
+
+		return <MenuLocations locations={locations} />
+	}
+
+	const renderMenu = () => {
+		return <MenuItems items={appPages}/>
+	}
 
 	return (
 		<IonMenu contentId="main" type="overlay">
@@ -67,31 +54,11 @@ const Menu = () => {
 					<IonItem lines="none">
 						<IonToggle checked={toggle} onIonChange={toggleFunction} />
 					</IonItem>
-					{appPages.map((appPage, index) => {
-						return (
-							<IonMenuToggle key={index} autoHide={false}>
-								<IonItem
-									className={location.pathname === appPage.url ? "selected" : ""}
-									routerLink={appPage.url}
-									routerDirection="none"
-									lines="none"
-									detail={false}
-								>
-									<IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
-									<IonLabel>{appPage.title}</IonLabel>
-								</IonItem>
-							</IonMenuToggle>
-						);
-					})}
+					{renderMenu()}
 				</IonList>
 				<IonList id="labels-list">
 					<IonListHeader>Città</IonListHeader>
-					{locations.map(city => (
-						<IonItem lines="none" key={city.id}>
-							<IonIcon slot="start" icon={bookmarkOutline} />
-							<IonLabel>{city.name}</IonLabel>
-						</IonItem>
-					))}
+					{renderLocationsSaved()}
 				</IonList>
 			</IonContent>
 		</IonMenu>

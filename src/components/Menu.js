9ce3fca89ea424230def7e3a418c57c77e74633a
Andrea <andrea@zampierolo.dev>
2022-01-07 11:48:47 +0100

    Menu items in config

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,50 +1,36 @@
-import { IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle, IonToggle } from "@ionic/react";
-
-import { useLocation } from "react-router-dom";
-import { ThemeContext } from "./themeContext";
-import {
-	bookmarkOutline,
-	mailOutline,
-	mailSharp,
-	paperPlaneOutline,
-	paperPlaneSharp,
-} from "ionicons/icons";
-import "./Menu.css";
 import { useContext } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { IonButton, IonContent, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonToggle } from "@ionic/react";
+
+import { ThemeContext } from "./themeContext";
+
 import MenuLocations from "./menu/menuLocations";
 import { MenuItems } from "./menu/menuItems";
+import { appPages } from "utils/config";
 
+import { resetStore, setCurrentLocation } from "store/actions";
 
-const appPages = [
-	{
-		title: "Today",
-		url: "/",
-		iosIcon: mailOutline,
-		mdIcon: mailSharp,
-	},
-	{
-		title: "Graph",
-		url: "/graph",
-		iosIcon: paperPlaneOutline,
-		mdIcon: paperPlaneSharp,
-	},
-];
+import "./Menu.css";
+
 
 const Menu = () => {
-	
 	const { locations } = useSelector(state => state.app);
 	const { toggle, toggleFunction } = useContext(ThemeContext);
+	const dispatch = useDispatch();
 
 	const renderLocationsSaved = () => {
 		if (!locations) return;
 
-		return <MenuLocations locations={locations} />
+		return <MenuLocations locations={locations} selectCity={cityHandler} />
 	}
 
 	const renderMenu = () => {
-		return <MenuItems items={appPages}/>
+		return <MenuItems items={appPages} />
 	}
+
+	const cityHandler = (location) => dispatch(setCurrentLocation(location))
+
+	const resetHandler = () => dispatch(resetStore())
 
 	return (
 		<IonMenu contentId="main" type="overlay">
@@ -52,13 +38,26 @@ const Menu = () => {
 				<IonList id="inbox-list">
 					<IonListHeader>My Meteo</IonListHeader>
 					<IonItem lines="none">
+						<IonLabel>DarkMode</IonLabel>
 						<IonToggle checked={toggle} onIonChange={toggleFunction} />
+					</IonItem>
+					<IonItem lines="none">
+						<IonLabel>Reset Store</IonLabel>
+						<IonButton
+							shape="round"
+							fill="outline"
+							onClick={() => resetHandler()}
+						>
+							Reset
+						</IonButton>
 					</IonItem>
 					{renderMenu()}
 				</IonList>
 				<IonList id="labels-list">
 					<IonListHeader>Città</IonListHeader>
-					{renderLocationsSaved()}
+					<div className="container">
+						{renderLocationsSaved()}
+					</div>
 				</IonList>
 			</IonContent>
 		</IonMenu>
---
+++ src/utils/config.js
@@ -0,0 +1,16 @@
+import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp } from "ionicons/icons";
+
+export const appPages = [
+	{
+		title: "Today",
+		url: "/",
+		iosIcon: mailOutline,
+		mdIcon: mailSharp,
+	},
+	{
+		title: "Graph",
+		url: "/graph",
+		iosIcon: paperPlaneOutline,
+		mdIcon: paperPlaneSharp,
+	},
+];

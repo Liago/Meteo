09ff3c3653443f793349ec22d90c23146fc88905
Andrea <andrea@zampierolo.dev>
2022-01-26 14:20:25 +0100

    Added css support on Minicard

--- src/components/UI/miniCard.js
+++ src/components/UI/miniCard.js
@@ -1,12 +1,14 @@
 import WeatherIcon from "./weatherIcon";
 import { MiniCardContainer } from './cardContainer';
 
-const MiniCardInfo = ({ icon, label, value, unit }) => {
+const MiniCardInfo = ({ icon, label, value, unit, fontSize }) => {
 	return (
-		<MiniCardContainer>
-			<WeatherIcon icon={icon} />
-			<span className="font-light"> {label}</span>
-			<p className="text-center pt-2">{value}{unit}</p>
+		<MiniCardContainer >
+			<div className={fontSize}>
+				<WeatherIcon icon={icon} />
+				<span className="font-light"> {label}</span>
+				<p className="text-center pt-2">{value}{unit}</p>
+			</div>
 		</MiniCardContainer>
 	)
 }

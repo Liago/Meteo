63133dc3850affcdc974ce65310e4828cbf06fec
Andrea <andrea@zampierolo.dev>
2022-02-14 17:00:58 +0100

    set environment

--- set-env.js
+++ set-env.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const environmentFile = `export const endpoint = {
-	WEATHER_FORECAST: '${process.env.WEATHER_FORECAST_ENDPOINT}',
+	RAPID_API: '${process.env.RAPID_API_ENDPOINT}',
 	LOCATION_IQ: '${process.env.LOCATIONIQ_ENDPOINT}',
 	test_mode: ${process.env.DEBUG},
 };
@@ -9,7 +9,6 @@ const environmentFile = `export const endpoint = {
 export const api_keys = {
 		LOCATION_IQ: '${process.env.LOCATIONIQ_TOKEN}',
 		RAPID_API: '${process.env.RAPID_API_KEY}',
-		RAPID_API_HOST: '${process.env.RAPID_API_HOST}',
 	}
 `;
 
